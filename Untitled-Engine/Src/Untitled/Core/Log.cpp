67f80505abbc361de53ae1bac5d3f386ba284846
Work Account <szy.jackiewicz@gmail.com>
2020-01-04 02:10:32 +0000

    Fix indentation style

--- Untitled-Engine/Src/Untitled/Core/Log.cpp
+++ Untitled-Engine/Src/Untitled/Core/Log.cpp
@@ -11,16 +11,17 @@
 
 namespace ue {
 
-    std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-    std::shared_ptr<spdlog::logger> Log::s_AppLogger;
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 
-    void Log::Init() {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
-        s_CoreLogger->set_level(spdlog::level::trace);
+	std::shared_ptr<spdlog::logger> Log::s_AppLogger;
 
-        s_AppLogger = spdlog::stdout_color_mt("APP");
-        s_AppLogger->set_level(spdlog::level::trace);
-    }
-	
+	void Log::Init() {
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_AppLogger = spdlog::stdout_color_mt("APP");
+		s_AppLogger->set_level(spdlog::level::trace);
+	}
+
 }
